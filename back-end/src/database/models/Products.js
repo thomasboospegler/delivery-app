748ca35065a9f31c6f40bd89b9ba4775e43fef10
cristiano <crizatip@gmail.com>
2023-02-27 19:17:41 -0300

    feat: create sequelize models

---
+++ back-end/src/database/models/Products.js
@@ -0,0 +1,30 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const Products = sequelize.define('Products', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    urlImage: DataTypes.STRING,
+  },
+  {
+    tableName: 'products',
+    underscored: true,
+    timestamps: false,
+  },
+  );
+  
+  Products.associate = (models) => {
+    Products.hasMany(models.SalesProducts,
+      {
+        as: 'product',
+        foreignKey: 'productId',
+      },
+    );
+  };
+  return Products;
+}
