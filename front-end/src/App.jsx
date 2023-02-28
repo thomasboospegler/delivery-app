2f91c8e5c41c80e2a8586c0d88ace7e759d7670a
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-02-28 19:46:56 -0300

    fix: seeder

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/register" component={ Register } />
+        <Route exact path="/customer/products" />
       </Switch>
     </div>
   );
