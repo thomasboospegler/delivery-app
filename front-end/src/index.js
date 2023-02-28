20f0a5ea51af16c5996511fa58233878b02b27a2
Thomas Boos Pegler <tboospegler@gmail.com>
2023-02-28 16:40:38 -0400

    fix: users seeder

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,9 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
