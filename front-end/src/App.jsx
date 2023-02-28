742f1532a78922771a3d8217dfb3c2b9072f90e6
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-02-28 16:38:54 -0300

    feat: create register component with bug

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Switch>
+        <Route exact path="/register" component={ Register } />
+      </Switch>
     </div>
   );
 }
---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Register({ history }) {
+  const [userRegister, setUserRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [disabled, setDisable] = useState(true);
+
+  const PASSWORD_MIN_LENGTH = 5;
+  const NAME_MIN_LENGTH = 11;
+
+  const validateEntry = () => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (emailRegex.test(userRegister.email)
+    && userRegister.password.length >= PASSWORD_MIN_LENGTH
+    && userRegister.name.length >= NAME_MIN_LENGTH) {
+      console.log('if');
+      setDisable(false);
+    } else {
+      console.log(userRegister.password.length);
+      setDisable(true);
+    }
+  };
+
+  const handleChage = (e) => {
+    const { name, value } = e.target;
+    validateEntry();
+    setUserRegister((previous) => ({
+      ...previous,
+      [name]: value,
+    }));
+  };
+
+  const submitClick = async () => {
+    history.push('/');
+  };
+
+  useEffect(() => {
+
+  }, []);
+  return (
+    <div className="register-container">
+      <label htmlFor="nameRegister">
+        Nome:
+        <input
+          data-test-id="common_register__input-name"
+          type="text"
+          id="nameRegister"
+          name="name"
+          value={ userRegister.name }
+          onChange={ (e) => handleChage(e) }
+          required
+        />
+      </label>
+      <label htmlFor="emailRegister">
+        Email:
+        <input
+          data-test-id="common_register__input-email"
+          type="text"
+          id="email"
+          name="email"
+          value={ userRegister.email }
+          onChange={ (e) => handleChage(e) }
+          required
+        />
+      </label>
+      <label htmlFor="passwordRegister">
+        Senha:
+        <input
+          data-test-id="common_register__input-password"
+          type="text"
+          id="passwordRegister"
+          name="password"
+          value={ userRegister.password }
+          onChange={ (e) => handleChage(e) }
+          required
+        />
+      </label>
+      <button
+        data-test-id="common_register__button-register"
+        type="button"
+        onClick={ submitClick }
+        disabled={ disabled }
+      >
+        Cadastrar
+      </button>
+      <span data-test-id="common_register__element-invalid_register">
+        Elemento oculto (Mensagens de erro)
+      </span>
+    </div>
+  );
+}
+
+Register.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
