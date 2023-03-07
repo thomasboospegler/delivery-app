36240b0409950622a6837f4cd2982d8ec58b3e42
cristiano <crizatip@gmail.com>
2023-03-07 15:15:25 -0300

    fix: lint backend fix

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,7 +4,7 @@ const routes = require('../routes/routes');
 const app = express();
 app.use(express.json());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
