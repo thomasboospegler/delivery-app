712c7418c40c9c18c8f3d9b81fbe01c31c1cdda3
Thomas Boos Pegler <tboospegler@gmail.com>
2023-03-09 09:28:01 -0400

    refactor: correctotal price format in porders

--- front-end/src/components/CustomerOrderCard.jsx
+++ front-end/src/components/CustomerOrderCard.jsx
@@ -6,6 +6,7 @@ export default function CustomerOrderCard({ order, i }) {
   const history = useHistory();
   const { totalPrice, id, saleDate, status } = order;
   const date = new Intl.DateTimeFormat('pt-BR').format(new Date(saleDate));
+  const total = totalPrice.replace('.', ',');
   return (
     <button
       key={ `order-${i}` }
@@ -13,10 +14,10 @@ export default function CustomerOrderCard({ order, i }) {
       data-testid={ `customer_orders__element-order-id-${id}` }
       onClick={ () => history.push(`/customer/orders/${id}`) }
     >
-      <p data-testid={ `customer_orders__element-order-id-${id}` }>{`Pedido ${id}`}</p>
+      <p data-testid={ `customer_orders__element-order-id-${id}` }>{`Pedido ${i + 1}`}</p>
       <p data-testid={ `customer_orders__element-delivery-status-${id}` }>{status}</p>
       <p data-testid={ `customer_orders__element-order-date-${id}` }>{date}</p>
-      <p data-testid={ `customer_orders__element-card-price-${id}` }>{totalPrice}</p>
+      <p data-testid={ `customer_orders__element-card-price-${id}` }>{total}</p>
     </button>
   );
 }
